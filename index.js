ff008126415b67c79081b480a42fce8c5d83e440
Quazi Nafiul Islam <gamesbrainiac@gmail.com>
2015-10-22 21:06:45 +0600

    Adding obvious mistakes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  var x = 10;
+
   // Bad Practice, JSHint should catch this
   document.write("We're doing a bad thing here. We shouldn't" +
                  " be using document.write.");
